dd81263553aff3af747145126ee525278ca823a0
Marco Roa <marco@homesusa.com>
2021-03-03 20:36:23 +0100

    Added api configurations extensions class and added the project for linq extensions

--- Husa.Extensions.Api/MvcConfigurationExtensions.cs
+++ Husa.Extensions.Api/MvcConfigurationExtensions.cs
@@ -4,14 +4,13 @@ namespace Husa.Extensions.Api
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.Extensions.DependencyInjection;
 
-    public static class Configuration
+    public static class MvcConfigurationExtensions
     {
-        public static MvcOptions AddControllerPrefixConventions(this MvcOptions options,string prefix, params string[] excluded)
+        public static MvcOptions AddControllerPrefixConventions(this MvcOptions options, string prefix, params string[] excluded)
         {
             options.Conventions.Add(new RoutePrefixConvention(prefix, excluded));
 
             return options;
-
         }
     }
 }
