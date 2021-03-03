780fbb02a243aa6c65a61ebbb9522f825f8aaa43
Marco Roa <marco@homesusa.com>
2021-03-03 20:51:17 +0100

    Added enum extensions and string extensions class

---
+++ Husa.Extensions.Common/StringExtensions.cs
@@ -0,0 +1,29 @@
+namespace Husa.Extensions.Common
+{
+    using System;
+    using System.Text;
+    using Microsoft.AspNetCore.WebUtilities;
+
+    public static class StringExtensions
+    {
+        public static string EncodeToBase64Url(this string token)
+        {
+            if (string.IsNullOrEmpty(token))
+            {
+                throw new ArgumentNullException(nameof(token));
+            }
+
+            return WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(token));
+        }
+
+        public static string DecodeFromBase64Url(this string token)
+        {
+            if (string.IsNullOrEmpty(token))
+            {
+                throw new ArgumentNullException(nameof(token));
+            }
+
+            return Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(token));
+        }
+    }
+}
