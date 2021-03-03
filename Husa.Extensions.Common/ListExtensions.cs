40d6da5202c715afaa60ed3ff45a1b3e63a21bc6
Marco Roa <marco@homesusa.com>
2021-03-03 21:06:10 +0100

    Added datetime extensions and list extensions

---
+++ Husa.Extensions.Common/ListExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace Husa.Extensions.Common
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public static class ListExtensions
+    {
+        public static IEnumerable<int> ToInt(this List<string> elements)
+        {
+            return elements.Select(intElement =>
+                {
+                    _ = int.TryParse(intElement, out var intValue);
+                    return intValue;
+                }).ToList();
+        }
+    }
+}
