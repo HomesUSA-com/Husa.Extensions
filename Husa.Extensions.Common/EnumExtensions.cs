adc16376a9996559cba76e5265221b41007a2e26
Israel Marquez <imarquez@homesusa.com>
2022-09-01 14:36:46 +0000

    Merged in feature/HRP-2480 (pull request #55)
    HRP-2480 Enum extension helpers added to Common
    
    * HRP-2480 Enum extension helpers added to Common
    
    
    Approved-by: Marco Roa
    Approved-by: Javier Mendez

--- Husa.Extensions.Common/EnumExtensions.cs
+++ Husa.Extensions.Common/EnumExtensions.cs
@@ -1,6 +1,8 @@
 namespace Husa.Extensions.Common
 {
     using System;
+    using System.ComponentModel;
+    using System.Linq;
 
     public static class EnumExtensions
     {
@@ -9,6 +11,45 @@ namespace Husa.Extensions.Common
         {
             _ = Enum.TryParse<TEnum>(enumElement, true, out var enumValue);
             return enumValue;
+        }
+
+        public static string GetEnumDescription(this Enum enumeration)
+        {
+            var type = enumeration.GetType();
+            var memberInfo = type.GetMember(enumeration.ToString()).FirstOrDefault();
+
+            if (memberInfo == null)
+            {
+                throw new InvalidOperationException($"Unable to get memberinfo for enum '{type.FullName}'");
+            }
+
+            var attribute = memberInfo.GetCustomAttributes(typeof(DescriptionAttribute), false).FirstOrDefault();
+
+            return ((DescriptionAttribute)attribute)?.Description ?? throw new InvalidOperationException($"Unable to get memberinfo for enum '{type.FullName}'");
+        }
+
+        public static TEnum GetEnumValueFromDescription<TEnum>(this string enumValue)
+            where TEnum : struct
+        {
+            var type = typeof(TEnum);
+            var membersInfo = type.GetMembers();
+
+            if (!membersInfo.Any())
+            {
+                throw new InvalidOperationException($"Unable to get memberinfo for enum '{type.FullName}'");
+            }
+
+            var memberInfo = membersInfo
+                .SingleOrDefault(m => m
+                    .GetCustomAttributes(typeof(DescriptionAttribute), false)
+                    .FirstOrDefault(attribute => ((DescriptionAttribute)attribute).Description == enumValue) != null);
+
+            if (memberInfo == null)
+            {
+                throw new InvalidOperationException($"Unable to get memberinfo for enum '{type.FullName}'");
+            }
+
+            return (TEnum)Enum.Parse(typeof(TEnum), memberInfo.Name);
         }
     }
 }
