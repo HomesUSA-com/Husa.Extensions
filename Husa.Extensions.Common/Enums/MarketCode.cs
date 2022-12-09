0d9f6253ee2a4cd386faf83f13fe47805836224f
Israel Marquez <imarquez@homesusa.com>
2022-12-09 17:14:13 +0000

    Merged in feature/HRP-2845 (pull request #81)
    HRP-2845 enum description added to marketcode
    
    * HRP-2845 enum description added to marketcode
    
    
    Approved-by: Javier Mendez

--- Husa.Extensions.Common/Enums/MarketCode.cs
+++ Husa.Extensions.Common/Enums/MarketCode.cs
@@ -1,22 +1,30 @@
 namespace Husa.Extensions.Common.Enums
 {
+    using System.ComponentModel;
     using System.ComponentModel.DataAnnotations;
 
     public enum MarketCode
     {
         [Display(Name = "DFW")]
+        [Description("DFW")]
         DFW,
         [Display(Name = "Austin")]
+        [Description("Austin")]
         Austin,
         [Display(Name = "San Antonio")]
+        [Description("SanAntonio")]
         SanAntonio,
         [Display(Name = "Houston")]
+        [Description("Houston")]
         Houston,
         [Display(Name = "Longview")]
+        [Description("Longview")]
         Longview,
         [Display(Name = "Tyler")]
+        [Description("Tyler")]
         Tyler,
         [Display(Name = "Waco")]
+        [Description("Waco")]
         Waco,
     }
 }
