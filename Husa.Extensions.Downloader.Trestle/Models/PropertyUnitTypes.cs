883da4a11e57129097e20bf7a8c4558f2f23abe0
Isaac Gonzalez <isaac@homesusa.com>
2023-01-26 21:00:40 -0400

    feature/HRP-2957 - Jsonconverters added to models

--- Husa.Extensions.Downloader.Trestle/Models/PropertyUnitTypes.cs
+++ Husa.Extensions.Downloader.Trestle/Models/PropertyUnitTypes.cs
@@ -1,46 +1,53 @@
 namespace Husa.Extensions.Downloader.Trestle.Models
 {
     using System;
+    using System.Collections.Generic;
+    using System.Text.Json.Serialization;
+    using Husa.Extensions.Downloader.Trestle.Helpers.Converters;
     using Husa.Extensions.Downloader.Trestle.Models.Enums;
 
     public class PropertyUnitTypes
     {
-        public bool HumanModifiedYN { get; set; }
+        public bool? HumanModifiedYN { get; set; }
         public string ListAgentKey { get; set; }
         public string ListingId { get; set; }
         public string ListingKey { get; set; }
-        public int ListingKeyNumeric { get; set; }
+        public int? ListingKeyNumeric { get; set; }
         public string ListOfficeKey { get; set; }
         public DateTimeOffset ModificationTimestamp { get; set; }
         public string OriginatingSystemName { get; set; }
         public string OriginatingSystemSubName { get; set; }
+        [JsonConverter(typeof(StringNullableEnumConverter<Permission?>))]
         public Permission? Permission { get; set; }
         public string PermissionPrivate { get; set; }
-        public decimal UnitTypeActualRent { get; set; }
+        public decimal? UnitTypeActualRent { get; set; }
         public string UnitTypeActualRentRange { get; set; }
-        public decimal UnitTypeArea { get; set; }
-        public int UnitTypeBathsTotal { get; set; }
-        public int UnitTypeBedsTotal { get; set; }
-        public decimal UnitTypeDeposit { get; set; }
+        public decimal? UnitTypeArea { get; set; }
+        public int? UnitTypeBathsTotal { get; set; }
+        public int? UnitTypeBedsTotal { get; set; }
+        public decimal? UnitTypeDeposit { get; set; }
         public string UnitTypeDescription { get; set; }
-        public bool UnitTypeFireplaceYN { get; set; }
+        public bool? UnitTypeFireplaceYN { get; set; }
+        [JsonConverter(typeof(StringNullableEnumConverter<Furnished?>))]
         public Furnished? UnitTypeFurnished { get; set; }
-        public bool UnitTypeGarageAttachedYN { get; set; }
-        public decimal UnitTypeGarageSpaces { get; set; }
+        public bool? UnitTypeGarageAttachedYN { get; set; }
+        public decimal? UnitTypeGarageSpaces { get; set; }
         public string UnitTypeKey { get; set; }
-        public int UnitTypeKeyNumeric { get; set; }
-        public bool UnitTypeLeasedYN { get; set; }
+        public int? UnitTypeKeyNumeric { get; set; }
+        public bool? UnitTypeLeasedYN { get; set; }
         public DateTime UnitTypeLeaseExpires { get; set; }
-        public bool UnitTypeMonthToMonthYN { get; set; }
-        public int UnitTypeNumFullBaths { get; set; }
-        public int UnitTypeNumHalfBaths { get; set; }
+        public bool? UnitTypeMonthToMonthYN { get; set; }
+        public int? UnitTypeNumFullBaths { get; set; }
+        public int? UnitTypeNumHalfBaths { get; set; }
+        [JsonConverter(typeof(StringNullableEnumConverter<UnitTypeOccupantType?>))]
         public UnitTypeOccupantType? UnitTypeOccupantType { get; set; }
-        public decimal UnitTypePetDeposit { get; set; }
-        public bool UnitTypePetDepositPerPetYN { get; set; }
-        public int UnitTypeProForma { get; set; }
-        public decimal UnitTypeTotalRent { get; set; }
-        public UnitTypeType? UnitTypeType { get; set; }
+        public decimal? UnitTypePetDeposit { get; set; }
+        public bool? UnitTypePetDepositPerPetYN { get; set; }
+        public int? UnitTypeProForma { get; set; }
+        public decimal? UnitTypeTotalRent { get; set; }
+        [JsonConverter(typeof(StringListEnumConverter<UnitTypeType>))]
+        public IEnumerable<UnitTypeType> UnitTypeType { get; set; }
         public string UnitTypeUnitNum { get; set; }
-        public int UnitTypeUnitsTotal { get; set; }
+        public int? UnitTypeUnitsTotal { get; set; }
     }
 }
