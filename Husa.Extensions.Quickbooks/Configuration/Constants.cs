7e88e1c7c5ee38ea1a21ce1704482bdf85176d76
IsraelMarquez <imarquez@homesusa.com>
2023-12-19 07:23:45 -0300

    HRP-5137 Move common code to constants

---
+++ Husa.Extensions.Quickbooks/Configuration/Constants.cs
@@ -0,0 +1,15 @@
+namespace Husa.Extensions.Quickbooks.Configuration
+{
+    public static class Constants
+    {
+        public const string SalesItemLineId = "3";
+        public const string ClassRefId = "7000000000000674718";
+        public const string InvoiceDetailType = "SalesItemLineDetail";
+        public const string EntityTypeInvoice = "Invoice";
+        public const string ReListedName = "Relisted";
+        public const string ComparableName = "Comparable";
+        public const string NewListingName = "New Listing";
+        public const string ActiveTransferName = "Active Transfer";
+        public const string PendingTransferName = "Pending Transfer";
+    }
+}
