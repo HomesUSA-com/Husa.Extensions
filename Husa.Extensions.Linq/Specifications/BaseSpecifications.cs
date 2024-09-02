792d95d7be4945b625dd90fc4a6da9b3a8eb5bdc
Andrea Centeno <andrea@homesusa.com>
2024-09-02 16:27:29 +0000

    Merged in feature/HRP-6827 (pull request #212)
    HRP-6827 ApplyPaginationFilter for IEnumerable
    
    * HRP-6827 ApplyPaginationFilter for IEnumerable
    
    
    Approved-by: Wladimir

--- Husa.Extensions.Linq/Specifications/BaseSpecifications.cs
+++ Husa.Extensions.Linq/Specifications/BaseSpecifications.cs
@@ -1,10 +1,21 @@
 namespace Husa.Extensions.Linq.Specifications
 {
+    using System.Collections.Generic;
     using System.Linq;
 
     public static class BaseSpecifications
     {
         public static IQueryable<T> ApplyPaginationFilter<T>(this IQueryable<T> records, int skip, int take, bool avoidPagination = false)
+        {
+            if (avoidPagination || skip < 0 || take < 1)
+            {
+                return records;
+            }
+
+            return records.Skip(skip * take).Take(take);
+        }
+
+        public static IEnumerable<T> ApplyPaginationFilter<T>(this IEnumerable<T> records, int skip, int take, bool avoidPagination = false)
         {
             if (avoidPagination || skip < 0 || take < 1)
             {
