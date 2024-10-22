c554499095eb3c1aadadf6012cc0a2b94dce7b98
Marco Roa <marco@homesusa.com>
2024-10-22 17:31:23 +0200

    Implemented the IAsyncDisposable pattern in the sender factory

--- Husa.Extensions.ServiceBus/Services/ServiceBusSenderFactory.cs
+++ Husa.Extensions.ServiceBus/Services/ServiceBusSenderFactory.cs
@@ -2,15 +2,17 @@ namespace Husa.Extensions.ServiceBus.Services;
 
 using System;
 using System.Collections.Concurrent;
+using System.Threading.Tasks;
 using Azure.Messaging.ServiceBus;
 using Husa.Extensions.ServiceBus.Interfaces;
 using Microsoft.Extensions.Logging;
 
-public class ServiceBusSenderFactory : IServiceBusSenderFactory
+public class ServiceBusSenderFactory : IServiceBusSenderFactory, IAsyncDisposable
 {
     private readonly ServiceBusClient serviceBusClient;
     private readonly ILogger<ServiceBusSenderFactory> logger;
     private readonly ConcurrentDictionary<string, ServiceBusSender> senders = new();
+    private bool disposed;
 
     public ServiceBusSenderFactory(ServiceBusClient serviceBusClient, ILogger<ServiceBusSenderFactory> logger)
     {
@@ -27,5 +29,39 @@ public class ServiceBusSenderFactory : IServiceBusSenderFactory
 
         this.logger.LogInformation("Getting sender for topic {Topic}", topicName);
         return this.senders.GetOrAdd(topicName, this.serviceBusClient.CreateSender);
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        await this.DisposeAsyncCore();
+
+        // Suppress finalization.
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual async ValueTask DisposeAsyncCore()
+    {
+        if (this.disposed)
+        {
+            return;
+        }
+
+        // Dispose all the senders asynchronously
+        foreach (var (topic, sender) in this.senders)
+        {
+            try
+            {
+                await sender.DisposeAsync();
+            }
+            catch (Exception ex)
+            {
+                this.logger.LogError(ex, "Failed to dispose sender for topic {Topic}", topic);
+            }
+        }
+
+        // Clear the dictionary
+        this.senders.Clear();
+
+        this.disposed = true;
     }
 }
