e1384af244e44b1d1c85c805a5aa964999b9f268
Andrea Centeno <andrea@homesusa.com>
2024-12-20 20:00:46 +0000

    Merged in feature/HRP-7666 (pull request #247)
    add RequestCorrelationIdMiddleware to service bus nuget
    
    * add RequestCorrelationIdMiddleware to service bus nuget
    
    
    Approved-by: Isaac Gonzalez

---
+++ Husa.Extensions.ServiceBus/Middlewares/RequestCorrelationIdMiddleware.cs
@@ -0,0 +1,27 @@
+namespace Husa.Extensions.ServiceBus.Middlewares
+{
+    using System;
+    using System.Threading.Tasks;
+    using Husa.Extensions.ServiceBus.Interfaces;
+    using Microsoft.AspNetCore.Http;
+
+    public class RequestCorrelationIdMiddleware
+    {
+        private readonly RequestDelegate next;
+        public RequestCorrelationIdMiddleware(RequestDelegate next)
+        {
+            this.next = next ?? throw new ArgumentNullException(nameof(next));
+        }
+
+        public Task InvokeAsync(HttpContext context, IConfigureTraceId configureTraceId)
+        {
+            if (configureTraceId is null)
+            {
+                throw new ArgumentNullException(nameof(configureTraceId));
+            }
+
+            configureTraceId.SetTraceId();
+            return this.next(context);
+        }
+    }
+}
